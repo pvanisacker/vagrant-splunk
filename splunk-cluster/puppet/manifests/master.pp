b762e428341cf38e71a3d9e3312ee74063127ce1
Pieter Van Isacker <pieter@localhost>
2014-09-08 20:29:12 +0200

    Fixes for the search head

--- splunk-cluster/puppet/manifests/master.pp
+++ splunk-cluster/puppet/manifests/master.pp
@@ -1,3 +1,5 @@
 include system
 include splunk
 include splunk-master
+include splunk-app-sos
+include splunk-app-sideview-utils
--- splunk-cluster/puppet/manifests/search-head.pp
+++ splunk-cluster/puppet/manifests/search-head.pp
@@ -1,3 +1,6 @@
 include system
 include splunk
 include splunk-search-head
+include splunk-app-for-nix
+include splunk-app-sos
+include splunk-app-sideview-utils
